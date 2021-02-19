bd77435331b849753b33dbf9ef03a902090b8bbc
PRATIK DAS <78647354+void3808@users.noreply.github.com>
2021-02-19 20:22:28 +0530

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,4 +1,21 @@
 """
+The circuit uses the repition code analogy and implements entanglement syndrome measurement to detect, analyse and rectify the errors.
+At first the circuit creates a bell's pair, and then encodes "error gates" on each of the two code qubits. Then, two rounds of syndrome measurement is applied on the circuit. The first round detects the possibility of error from the link qubit, and then applies a bit-flip to one of the code qubits to eradicate the error from the circuit. Based on the output produced by the measurement operator, one can indentiy and analuse the errors in one of the code qubits.
+The code produces all the possible choices of the "error gates" on each of the code qubits.
+
+Analysing the output of the syndrome measurement:-
+The output produced is a python dictionary which contains bit strings in the form '00 00' as its keys, and the values of those keys represent the number of shots for which the key occured.
+1. The 00 and 11 to the far right of the key are the logical readouts, representing the logical 0 and logical 1, respectively.
+2. The 00, 01 or 11 to the far left of the dictionary key indicates the possible presence of errors and their successful correction.
+	2.1. 00 indicates that the code was unable to detect any error in the two rounds of the syndrome measurement.
+	2.2. 01 indicates that the code detected a possible error in one of the code qubits, and then was able to apply correction to the circuit.
+	2.3. 10 indicates that the code didn't detect any error in the first round, but some kind of unknown error was introduced into the circuit before the second measurement.
+	2.4. 11 inidcates that the code detected an error in one of the code qubits before the first round, but was unable to rectify the error in the circuit.
+	
+Note:- Please notice that the circuit is unable to detect errors if both the code qubits has been compromised. The circuit works best only when one of the code qubits has been affected.
+
+Following is the list of outputs the code produces upon succesful compilation.
+
 			========= circuit[ 0 ][ 0 ]=========
 
                   ┌───┐     ┌───┐ ░                      ░                ┌─┐»
@@ -231,10 +248,13 @@ round_link_bit: 2/════════════════════�
 «                   ║  1 
 «round_link_bit: 2/═╩════
 «                   1    
-{'00 00': 515, '00 11': 509}"""
+{'00 00': 515, '00 11': 509}
+"""
 
 import qiskit
 from qiskit import *
+
+# Contruct takes the circuit as a parameter input and applies one of the gates among identiy, bit-filp and phase-flip depending on the value of 'i' to the qubit number 'qubit' in the register 'reg'
 def switch(circuit, i, qubit, reg):
     if i==0:
         circuit.i(reg[qubit])
@@ -243,15 +263,14 @@ def switch(circuit, i, qubit, reg):
     elif i==2:
         circuit.z(reg[qubit])
     
-cq=QuantumRegister(2,'code_qubit')
-lq=QuantumRegister(1,'link_qubit')
-cb=ClassicalRegister(2,'code_bit')
-rlb=ClassicalRegister(2,'round_link_bit')
-#qc={}
-#print(list(range(3)))
-for i in list(range(3)): #
+cq=QuantumRegister(2,'code_qubit') 		# This register contains the 2 code qubits, which has to be checked by the circuit
+lq=QuantumRegister(1,'link_qubit') 		# This register contains the single link qubit, which acts as the ancilla bit and on which the error analysis is performed
+cb=ClassicalRegister(2,'code_bit') 		# This register contains the classical bits which store the logical readouts of the code qubits
+rlb=ClassicalRegister(2,'round_link_bit') 	# This register contains the classical bits, on which the syndrome measurements are performed in order to detect and correct the error introduced in the code qubits
+
+# Loop iterates through all the possible combinations of the "error gates" and implements them into the circuit
+for i in list(range(3)):
     for j in list(range(3)):
- #       print(i,j)
         qc=QuantumCircuit(cq,lq,cb,rlb)
         qc.h(cq[0])
         qc.cx(cq[0],cq[1])
